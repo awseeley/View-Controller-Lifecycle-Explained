baa1627c61e6be6558c56af089d48b5a53e8ebab
Andrew Seeley <awseeley@gmail.com>
2017-06-28 22:01:13 +1000

    View Controller Lifecycle Explained
    View Controller Lifecycle Explained

---
+++ ViewControllerLifeCycle/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  ViewControllerLifeCycle
+//
+//  Created by Andrew Seeley on 28/6/17.
+//  Copyright © 2017 Seemu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("viewDidLoad")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print("viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("viewDidAppear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print("viewWillDisappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print("viewDidDisappear")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
